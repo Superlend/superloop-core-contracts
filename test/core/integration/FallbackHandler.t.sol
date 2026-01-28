bf940600bf75b0e655006d89b5d0fb4a6c8629a7
Priyam Anand <priyam27.anand@gmail.com>
2026-01-28 13:25:35 +0530

    test: init tests for seed

--- test/core/integration/FallbackHandler.t.sol
+++ test/core/integration/FallbackHandler.t.sol
@@ -1,38 +1,38 @@
-// // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-// pragma solidity ^0.8.13;
+pragma solidity ^0.8.13;
 
-// import {IntegrationBase} from "./IntegrationBase.sol";
-// import {DataTypes} from "../../../src/common/DataTypes.sol";
-// import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-// import {DepositManager} from "../../../src/core/DepositManager/DepositManager.sol";
-// import {Errors} from "../../../src/common/Errors.sol";
-// import {MockPreliquidation} from "../../../src/mock/MockPreliquidation.sol";
+import {IntegrationBase} from "./IntegrationBase.sol";
+import {DataTypes} from "../../../src/common/DataTypes.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {DepositManager} from "../../../src/core/DepositManager/DepositManager.sol";
+import {Errors} from "../../../src/common/Errors.sol";
+import {MockPreliquidation} from "../../../src/mock/MockPreliquidation.sol";
 
-// contract FallbackHandlerTest is IntegrationBase {
-//     address public preliquidation;
-//     DataTypes.CallType public callType;
-//     bytes32 public key;
+contract FallbackHandlerTest is IntegrationBase {
+    address public preliquidation;
+    DataTypes.CallType public callType;
+    bytes32 public key;
 
-//     function setUp() public override {
-//         super.setUp();
-//         callType = DataTypes.CallType.DELEGATECALL;
+    function setUp() public override {
+        super.setUp();
+        callType = DataTypes.CallType.DELEGATECALL;
 
-//         preliquidation = address(new MockPreliquidation());
+        preliquidation = address(new MockPreliquidation());
 
-//         key =
-//             keccak256(abi.encodePacked(abi.encodeWithSelector(MockPreliquidation.preliquidate.selector), id, callType));
-//         vm.startPrank(admin);
-//         superloop.setFallbackHandler(key, preliquidation);
-//         vm.stopPrank();
-//     }
+        key =
+            keccak256(abi.encodePacked(abi.encodeWithSelector(MockPreliquidation.preliquidate.selector), id, callType));
+        vm.startPrank(admin);
+        superloop.setFallbackHandler(key, preliquidation);
+        vm.stopPrank();
+    }
 
-//     function test_fallbackHandler() public {
-//         assertEq(superloop.fallbackHandler(key), preliquidation);
+    function test_fallbackHandler() public {
+        assertEq(superloop.fallbackHandler(key), preliquidation);
 
-//         // make a call to superloop with context of IPreliquidation.preliquidate
-//         MockPreliquidation(address(superloop)).preliquidate(id, callType, "");
+        // make a call to superloop with context of IPreliquidation.preliquidate
+        MockPreliquidation(address(superloop)).preliquidate(id, callType, "");
 
-//         // this should not revert, not reverting means the decoding in fallback handler is working correctly
-//     }
-// }
+        // this should not revert, not reverting means the decoding in fallback handler is working correctly
+    }
+}
--- test/core/integration/InstantWithdraw.t.sol
+++ test/core/integration/InstantWithdraw.t.sol
@@ -1,74 +1,77 @@
-// // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-// pragma solidity ^0.8.13;
+pragma solidity ^0.8.13;
 
-// import {IntegrationBase} from "./IntegrationBase.sol";
-// import {DataTypes} from "../../../src/common/DataTypes.sol";
-// import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-// import {DepositManager} from "../../../src/core/DepositManager/DepositManager.sol";
-// import {console} from "forge-std/Test.sol";
-// import {Errors} from "../../../src/common/Errors.sol";
+import {IntegrationBase} from "./IntegrationBase.sol";
+import {DataTypes} from "../../../src/common/DataTypes.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {DepositManager} from "../../../src/core/DepositManager/DepositManager.sol";
+import {console} from "forge-std/Test.sol";
+import {Errors} from "../../../src/common/Errors.sol";
 
-// contract InstantWithdrawTest is IntegrationBase {
-//     function setUp() public override {
-//         super.setUp();
-//     }
+contract InstantWithdrawTest is IntegrationBase {
+    function setUp() public override {
+        super.setUp();
+    }
 
-//     function test_instantWithdraw() public {
-//         _seed();
+    function test_instantWithdraw() public {
+        _seed();
 
-//         uint256 userSharesBalanceBefore = superloop.balanceOf(admin);
-//         uint256 userTokenBalanceBefore = IERC20(XTZ).balanceOf(admin);
-//         uint256 treasurySharesBalanceBefore = superloop.balanceOf(treasury);
+        uint256 vaultScale = 10 ** environment.vaultAssetDecimals;
 
-//         vm.startPrank(admin);
-//         uint256 instantWithdrawAmount = 10 * XTZ_SCALE;
-//         superloop.withdraw(instantWithdrawAmount, admin, admin);
+        uint256 userSharesBalanceBefore = superloop.balanceOf(admin);
+        uint256 userTokenBalanceBefore = IERC20(environment.vaultAsset).balanceOf(admin);
+        uint256 treasurySharesBalanceBefore = superloop.balanceOf(treasury);
 
-//         uint256 userSharesBalanceAfter = superloop.balanceOf(admin);
-//         uint256 userTokenBalanceAfter = IERC20(XTZ).balanceOf(admin);
-//         uint256 treasurySharesBalanceAfter = superloop.balanceOf(treasury);
+        vm.startPrank(admin);
+        uint256 instantWithdrawAmount = 10 * vaultScale;
+        superloop.withdraw(instantWithdrawAmount, admin, admin);
 
-//         uint256 instantWithdrawFee = (instantWithdrawAmount * INSTANT_WITHDRAW_FEE) / 10_000;
-//         assertEq(userSharesBalanceBefore - userSharesBalanceAfter, instantWithdrawAmount * 100);
-//         assertEq(userTokenBalanceAfter - userTokenBalanceBefore, instantWithdrawAmount - instantWithdrawFee);
-//         assertEq(treasurySharesBalanceAfter - treasurySharesBalanceBefore, instantWithdrawFee * 100);
-//     }
+        uint256 userSharesBalanceAfter = superloop.balanceOf(admin);
+        uint256 userTokenBalanceAfter = IERC20(environment.vaultAsset).balanceOf(admin);
+        uint256 treasurySharesBalanceAfter = superloop.balanceOf(treasury);
 
-//     function test_instantRedeem() public {
-//         _seed();
+        uint256 instantWithdrawFee = (instantWithdrawAmount * INSTANT_WITHDRAW_FEE) / 10_000;
+        assertEq(userSharesBalanceBefore - userSharesBalanceAfter, instantWithdrawAmount * 100);
+        assertEq(userTokenBalanceAfter - userTokenBalanceBefore, instantWithdrawAmount - instantWithdrawFee);
+        assertEq(treasurySharesBalanceAfter - treasurySharesBalanceBefore, instantWithdrawFee * 100);
+    }
 
-//         uint256 userSharesBalanceBefore = superloop.balanceOf(admin);
-//         uint256 userTokenBalanceBefore = IERC20(XTZ).balanceOf(admin);
-//         uint256 treasurySharesBalanceBefore = superloop.balanceOf(treasury);
+    function test_instantRedeem() public {
+        _seed();
 
-//         vm.startPrank(admin);
-//         uint256 instantRedeemAmount = 10 * ONE_SHARE;
-//         superloop.redeem(instantRedeemAmount, admin, admin);
+        uint256 userSharesBalanceBefore = superloop.balanceOf(admin);
+        uint256 userTokenBalanceBefore = IERC20(environment.vaultAsset).balanceOf(admin);
+        uint256 treasurySharesBalanceBefore = superloop.balanceOf(treasury);
 
-//         uint256 userSharesBalanceAfter = superloop.balanceOf(admin);
-//         uint256 userTokenBalanceAfter = IERC20(XTZ).balanceOf(admin);
-//         uint256 treasurySharesBalanceAfter = superloop.balanceOf(treasury);
+        vm.startPrank(admin);
+        uint256 instantRedeemAmount = 10 * ONE_SHARE;
+        superloop.redeem(instantRedeemAmount, admin, admin);
 
-//         uint256 instantRedeemFee = (instantRedeemAmount * INSTANT_WITHDRAW_FEE) / 10_000;
-//         assertEq(userSharesBalanceBefore - userSharesBalanceAfter, instantRedeemAmount);
-//         assertEq(userTokenBalanceAfter - userTokenBalanceBefore, (instantRedeemAmount - instantRedeemFee) / 100);
-//         assertEq(treasurySharesBalanceAfter - treasurySharesBalanceBefore, instantRedeemFee);
-//     }
+        uint256 userSharesBalanceAfter = superloop.balanceOf(admin);
+        uint256 userTokenBalanceAfter = IERC20(environment.vaultAsset).balanceOf(admin);
+        uint256 treasurySharesBalanceAfter = superloop.balanceOf(treasury);
 
-//     function _seed() internal {
-//         uint256 seedAmount = 100 * XTZ_SCALE;
-//         deal(XTZ, admin, seedAmount);
+        uint256 instantRedeemFee = (instantRedeemAmount * INSTANT_WITHDRAW_FEE) / 10_000;
+        assertEq(userSharesBalanceBefore - userSharesBalanceAfter, instantRedeemAmount);
+        assertEq(userTokenBalanceAfter - userTokenBalanceBefore, (instantRedeemAmount - instantRedeemFee) / 100);
+        assertEq(treasurySharesBalanceAfter - treasurySharesBalanceBefore, instantRedeemFee);
+    }
 
-//         vm.startPrank(admin);
-//         IERC20(XTZ).approve(address(superloop), seedAmount);
-//         superloop.seed(seedAmount);
-//         vm.stopPrank();
+    function _seed() internal {
+        uint256 vaultScale = 10 ** environment.vaultAssetDecimals;
+        uint256 seedAmount = 100 * vaultScale;
+        deal(environment.vaultAsset, admin, seedAmount);
 
-//         uint256 totalSupply = superloop.totalSupply();
+        vm.startPrank(admin);
+        IERC20(environment.vaultAsset).approve(address(superloop), seedAmount);
+        superloop.seed(seedAmount);
+        vm.stopPrank();
 
-//         assertApproxEqAbs(totalSupply, seedAmount * 100, 100);
-//         assertEq(superloop.totalAssets(), seedAmount);
-//         assertEq(superloop.balanceOf(admin), totalSupply);
-//     }
-// }
+        uint256 totalSupply = superloop.totalSupply();
+
+        assertApproxEqAbs(totalSupply, seedAmount * 100, 100);
+        assertEq(superloop.totalAssets(), seedAmount);
+        assertEq(superloop.balanceOf(admin), totalSupply);
+    }
+}
